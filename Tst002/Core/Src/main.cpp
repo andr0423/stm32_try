2fe453e98b0fc13e23069d2ded222ef93cdf4779
andr0423 <andr0423@yandex.ru>
2022-06-02 05:42:02 +0300

    format main.cpp

--- Tst002/Core/Src/main.cpp
+++ Tst002/Core/Src/main.cpp
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.cpp
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.cpp
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -45,15 +45,11 @@
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 
-
-
 //mqtt
 ip_addr_t ip_addr;
-mqtt_client_t * client;
+mqtt_client_t *client;
 
 extern struct netif gnetif;
-
-
 
 /* USER CODE END PTD */
 
@@ -70,8 +66,6 @@ extern struct netif gnetif;
 
 /* USER CODE BEGIN PV */
 
-
-
 const char mqtt_server[15] = MY_MQTT_SERVER_IP_ADDRES;
 extern uint8_t IP_ADDRESS[4];
 extern uint8_t NETMASK_ADDRESS[4];
@@ -79,8 +73,6 @@ extern uint8_t GATEWAY_ADDRESS[4];
 
 static bool ready_flag = false;
 static bool mesurement_flag = false;
-
-
 
 /* USER CODE END PV */
 
@@ -95,27 +87,21 @@ static void MX_NVIC_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-
-
 MyBlinker mb_blue = MyBlinker(LD2_Pin);
 MyBlinker mb_red = MyBlinker(LD3_Pin);
-
 
 MyOled my_oled = MyOled();
 MyButton my_btn = MyButton(USR_BTN_GPIO_Port, USR_BTN_Pin);
 MySensor ms;
 DHT_sensor dht_22;
 
-
-
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -156,15 +142,13 @@ int main(void)
   MX_NVIC_Init();
   /* USER CODE BEGIN 2 */
 
-
-
   // dht11
   dht_22 = { DHT11_1_wire_GPIO_Port, DHT11_1_wire_Pin, DHT22, 0 };
 
   // bmp280/bme280
   ms = MySensor();
-  ms.set_bmp( &hi2c2 );
-  ms.set_dht( &dht_22 );
+  ms.set_bmp(&hi2c2);
+  ms.set_dht(&dht_22);
 
   // 0.96" OLED display init and test
   my_oled.init();
@@ -175,19 +159,15 @@ int main(void)
 
   // mqtt
   client = mqtt_client_new();
-  ipaddr_aton( mqtt_server, &ip_addr );
+  ipaddr_aton(mqtt_server, &ip_addr);
 
-  if(client != NULL) {
-	example_do_connect(client);
+  if (client != NULL) {
+    example_do_connect(client);
   }
 
   ready_flag = true;
 
-
-
   HAL_TIM_Base_Start_IT(&htim6);
-
-
 
   //
   //  Try to fix ETHERNET - OK!
@@ -202,34 +182,31 @@ int main(void)
   //    https://github.com/AnielShri/STM32H745_Ethernet/blob/master/Documentation/lwip_nortos.md
   //
 
-
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
-	MX_LWIP_Process();
+    MX_LWIP_Process();
 
-	if (mesurement_flag)
-	{
-		mesurement_flag = false;
+    if (mesurement_flag) {
+      mesurement_flag = false;
 
-		mb_red.on();
-		mb_blue.toggle();
+      mb_red.on();
+      mb_blue.toggle();
 
-		ms.get_data();
-		my_oled.set_tph( ms.temperature, ms.pressure, ms.humidity_dht );
-		my_oled.display();
+      ms.get_data();
+      my_oled.set_tph(ms.temperature, ms.pressure, ms.humidity_dht);
+      my_oled.display();
 
-		example_publish(client, NULL, &ms.temperature, &ms.pressure, &ms.humidity_dht );
-		example_do_connect(client);
+      example_publish(client, NULL, &ms.temperature, &ms.pressure,
+          &ms.humidity_dht);
+      example_do_connect(client);
 
-		mb_red.off();
-	}
+      mb_red.off();
+    }
 
     /* USER CODE BEGIN 3 */
   }
@@ -237,21 +214,20 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -262,97 +238,84 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 2;
   RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
   /** Activate the Over-Drive mode
-  */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
+   */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
     Error_Handler();
   }
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+      | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /**
-  * @brief NVIC Configuration.
-  * @retval None
-  */
-static void MX_NVIC_Init(void)
-{
-	  /* EXTI15_10_IRQn interrupt configuration */
-	  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 4, 0);
-	  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
-	  /* ETH_IRQn interrupt configuration */
-	  HAL_NVIC_SetPriority(ETH_IRQn, 2, 0);
-	  HAL_NVIC_EnableIRQ(ETH_IRQn);
-	  /* TIM6_DAC_IRQn interrupt configuration */
-	  HAL_NVIC_SetPriority(TIM6_DAC_IRQn, 3, 0);
-	  HAL_NVIC_EnableIRQ(TIM6_DAC_IRQn);
+ * @brief NVIC Configuration.
+ * @retval None
+ */
+static void MX_NVIC_Init(void) {
+  /* EXTI15_10_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 4, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+  /* ETH_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(ETH_IRQn, 2, 0);
+  HAL_NVIC_EnableIRQ(ETH_IRQn);
+  /* TIM6_DAC_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(TIM6_DAC_IRQn, 3, 0);
+  HAL_NVIC_EnableIRQ(TIM6_DAC_IRQn);
 }
 
 /* USER CODE BEGIN 4 */
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-	my_btn.catch_click();
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+  my_btn.catch_click();
 
-	if ( ready_flag && my_btn.is_Click() ){  // ready_flag false until super circle
+  if (ready_flag && my_btn.is_Click()) {  // ready_flag false until super circle
 
+    if (my_btn.is_Short()) {
+      my_oled.next_display();
+    } else if (my_btn.is_Middle()) {
+      my_oled.prev_display();
+    } else if (my_btn.is_Long()) {
+      if (my_oled.get_display() == 3) {
+        my_oled.oled_testing();
+      }
+    }
 
-		if ( my_btn.is_Short() )
-		{
-			my_oled.next_display();
-		}
-		else if ( my_btn.is_Middle() )
-		{
-			my_oled.prev_display();
-		}
-		else if ( my_btn.is_Long() )
-		{
-			if ( my_oled.get_display() == 3 ){
-				my_oled.oled_testing();
-			}
-		}
-
-		my_btn.clean();
-	}
+    my_btn.clean();
+  }
 }
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-	if(htim->Instance == TIM6) //check if the interrupt comes from TIM1
-	{
-		mesurement_flag = true;
-	}
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+  if (htim->Instance == TIM6) //check if the interrupt comes from TIM1
+  {
+    mesurement_flag = true;
+  }
 }
 
 /* USER CODE END 4 */
 
 /* MPU Configuration */
 
-void MPU_Config(void)
-{
-  MPU_Region_InitTypeDef MPU_InitStruct = {0};
+void MPU_Config(void) {
+  MPU_Region_InitTypeDef MPU_InitStruct = { 0 };
 
   /* Disables the MPU */
   HAL_MPU_Disable();
 
   /** Initializes and configures the Region and the memory to be protected
-  */
+   */
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
   MPU_InitStruct.Number = MPU_REGION_NUMBER0;
   MPU_InitStruct.BaseAddress = 0x30040000;
@@ -368,7 +331,7 @@ void MPU_Config(void)
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
 
   /** Initializes and configures the Region and the memory to be protected
-  */
+   */
   MPU_InitStruct.Number = MPU_REGION_NUMBER1;
   MPU_InitStruct.BaseAddress = 0x3004400;
   MPU_InitStruct.Size = MPU_REGION_SIZE_16KB;
@@ -382,18 +345,16 @@ void MPU_Config(void)
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGINrror_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
-    for( int i = 0 ; i < 2700000 ; i+=2 ){
-        i--;
+  while (1) {
+    for (int i = 0; i < 2700000; i += 2) {
+      i--;
     }
 
     mb_red.toggle();
